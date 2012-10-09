092688d9ff5db064d1fc917d291f72d5d3935e9f
sincere520 <wdapya@naver.com>
2012-10-09 11:46:12 +0900

    modi for FB test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	  String str = "1234";
-        System.out.println( "Hello World! " + Integer.parseInt(str) );
+    	  String arg = null;
+    	  System.out.println("Hello World! "+arg.equals("0"));
     }
 }
