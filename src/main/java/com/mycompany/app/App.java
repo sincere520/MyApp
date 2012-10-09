fcfeaa6559442663507041ff6a14c94db0a0fa21
sincere520 <wdapya@naver.com>
2012-10-09 13:15:54 +0900

    modi for FB test - deleted files

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,9 +1,5 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
@@ -11,6 +7,12 @@ public class App
   	  String str1 = null;
   	  String str2 = "str";
   	  System.out.println("Hello World! "+str1.equals(str2));
-  	  
+
+  	  String test1 = "123";
+  	  char[] test2 = null;
+  	  String.copyValueOf(test2);
+
+  	  System.out.println("Hello World! "+test1);
+  	  System.out.println("Hello World! "+test2);
     }
 }
