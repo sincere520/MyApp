c01f960e9138ed78fa358ed2ac82bdbe9ae3f672
sincere520 <wdapya@naver.com>
2012-10-09 13:03:37 +0900

    modi for FB test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,10 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-    	  String arg = null;
-    	  System.out.println("Hello World! "+arg.equals(1));
-    	  System.out.println("Hello World! "+arg.equals('1'));
-    	  System.out.println("Hello World! "+arg.equals("1"));
-    	  System.out.println("Hello World! "+arg.equals(null));
+  	  String str1 = null;
+  	  String str2 = "str";
+  	  System.out.println("Hello World! "+str1.equals("1"));
+  	  System.out.println("Hello World! "+str2.equals(null));
+  	System.out.println("Hello World! "+ Integer.parseInt(str2, 0));
+  	  
     }
 }
