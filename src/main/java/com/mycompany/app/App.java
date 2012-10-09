57ea71779357341c71f783c7c3cfb5b37735132e
sincere520 <wdapya@naver.com>
2012-10-09 13:49:19 +0900

    modi for FB test - updated mid level bug

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ public class App
   	  System.out.println("Hello World! "+str1.equalsIgnoreCase(str2));
 
   	  String test1 = "123";
-  	  char[] test2 = null;
+  	  char[] test2 = {'3','2','1'};
   	  test1 = String.copyValueOf(test2);
 
   	  System.out.println("Hello World! "+test1);
